1bc52250a48659756283cfa42f7b6549c146dcb3
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-14 16:35:19 +0000

    created fields for Item class

--- Item.java
+++ Item.java
@@ -1,2 +1,13 @@
 public class Item {
+    private int lotNumber;
+
+    private String nameOfBuyer;
+
+    private double priceSold;
+
+    private int yearSold;
+
+    private String itemType;
+
+
 }
