cbd9a51060d9bcb770d3ac18e94aa52596be7931
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-15 11:59:28 +0000

    defined fields for AuctionHouse class

--- AuctionHouse.java
+++ AuctionHouse.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuctionHouse {
+    private String auctionHouseName;
+
+    private List<Item> auctionHouseItems = new ArrayList<>();
+
+
 }
