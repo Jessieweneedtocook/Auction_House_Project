81b7a9e2ce130a7d6b4009bca1004ac91e75263d
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-20 16:39:28 +0000

    added a menu method to ReportingIO, uses scanner and reporting objects to method to allow inputs and auction houses to be added.

--- ReportingIO.java
+++ ReportingIO.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class ReportingIO {
+    public void menu() {
+        Scanner s = new Scanner(System.in);
+        Reporting r = new Reporting();
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("Menu");
+            System.out.println("---------------");
+            System.out.println("Option 1: Enter house data");
+            System.out.println("Option 2: Enter item data");
+            System.out.println("Option 3: Provide reporting data");
+            System.out.println("Exit");
+            int i = s.nextInt();
+            switch (i) {
+                case 1:
+
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
