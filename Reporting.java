de76ceb76c2759c7d614439e9a5ac28cebf085bc
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-20 14:52:20 +0000

    commented all fields and methods for Reporting class

--- Reporting.java
+++ Reporting.java
@@ -3,12 +3,15 @@ import java.util.List;
 
 public class Reporting {
 
+    // defined list of all auctionHouses
     private List<AuctionHouse> auctionHouses = new ArrayList<>();
 
+    // method that adds auction house to auctionHouses List
     public void addAuctionHouse(AuctionHouse a) {
         auctionHouses.add(a);
     }
 
+    // method that returns the highest prices item across all the auction houses
     public Item highestPrice(){
         if  (auctionHouses.isEmpty())
             return null;
@@ -21,6 +24,7 @@ public class Reporting {
         return maxPriceItem;
     }
 
+    // method that returns the auction house with the highest average price in a given year
     public AuctionHouse highestAvPrice(int year){
         if  (auctionHouses.isEmpty())
             return null;
@@ -33,6 +37,7 @@ public class Reporting {
         return maxAvPrice;
     }
 
+    // method that returns a list of all items with a price greater than a given value
     public List<Item> allPriceGreaterThan(double price){
         List<Item> greaterThan = new ArrayList<>();
         for (AuctionHouse a: auctionHouses) {
