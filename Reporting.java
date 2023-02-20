7c9fb8fab87a029399de198123ceaa9d643d3de9
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-20 13:03:11 +0000

    created a method, highestPrice, which returns the item with the highest price across all auction houses in auctionHouses.

--- Reporting.java
+++ Reporting.java
@@ -5,6 +5,21 @@ public class Reporting {
 
     private List<AuctionHouse> auctionHouses = new ArrayList<>();
 
-    public void addAuctionHouse(AuctionHouse a){
+    public void addAuctionHouse(AuctionHouse a) {
         auctionHouses.add(a);
+    }
+
+    public Item highestPrice(){
+        if  (auctionHouses.isEmpty())
+            return null;
+
+        Item maxPrice = auctionHouses.get(0).highestPriceItem();
+        for (AuctionHouse a: auctionHouses) {
+            if (a.highestPriceItem().getPriceSold() > maxPrice.getPriceSold())
+                maxPrice = a.highestPriceItem();
+        }
+        return maxPrice;
+    }
+
+
 }
