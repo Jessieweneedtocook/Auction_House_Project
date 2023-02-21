655486c9ce2f6e5f502d816c0a906fef5c334318
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2023-02-21 01:19:20 +0000

    created TestFile class that is used to test all the methods used in the auction house system. Created test method where ive added 5 auction houses as test data

---
+++ TestFile.java
@@ -0,0 +1,16 @@
+public class TestFile {
+
+    public void test() {
+        Reporting r = new Reporting();
+        r.addAuctionHouse(new AuctionHouse("House 0"));
+        r.addAuctionHouse(new AuctionHouse("House 1"));
+        r.addAuctionHouse(new AuctionHouse("House 2"));
+        r.addAuctionHouse(new AuctionHouse("House 3"));
+        r.addAuctionHouse(new AuctionHouse("House 4"));
+
+
+
+    }
+
+
+}
